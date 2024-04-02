847cc4a46d041df27abd44df183a82a21577ed54
Scaffolder <scaffolder@backstage.io>
2024-04-02 09:09:28 -0300

    initial commit

---
+++ src/main/java/com/sulimann/mercadolivre/utils/constants/Path.java
@@ -0,0 +1,11 @@
+package com.sulimann.mercadolivre.utils.constants;
+
+public final class Path {
+
+    public static final String INIT = "/init";
+
+    private Path() {
+        throw new AssertionError("Não é permitido instanciar esta classe.");
+    }
+
+}
